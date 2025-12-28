06d000b474c22f223d5848aee16f626f1955b656
cristiandias504 <cristiandias504@gmail.com>
2025-12-28 09:55:13 -0500

    Interactua con las salidas digitales, apagado mas controlado y modo Alarma implementado

--- Llave_v1.ino
+++ Llave_v1.ino
@@ -59,6 +59,10 @@ int estadoActual = 0;
 
 int desplazamiento = 0;
 
+bool conexionValida = false;
+bool procesoEncender = false;
+bool procesoParpadeo = true;
+
 int ArrayA[] = { 6, 2, 5, 1, 4, 9, 8, 7, 3 };  // Desplazar X+Num de la izquierda a la derecha
 int ArrayB[] = { 2, 9, 8, 4, 6, 1, 5, 3, 7 };
 int ArrayC[] = { 5, 3, 6, 7, 2, 9, 4, 8, 1 };
@@ -71,7 +75,7 @@ void callback_function(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
     SerialBT.println(claveCifrada);
   } else if (event == ESP_SPP_CLOSE_EVT) {
     Serial.println("Cliente desconectado");
-    procesoApagado(1);
+    procesoApagado(3);
   }
 }
 
@@ -95,7 +99,7 @@ void setup() {
   digitalWrite(principal, LOW);
   digitalWrite(direccion, LOW);
   digitalWrite(led_rojo, LOW);
-  digitalWrite(lm2596, HIGH); //LOW);
+  digitalWrite(lm2596, LOW);
   digitalWrite(sirena, LOW);
 
   SerialBT.begin("ESP32");  //NOMBRE COMO SE HARA VISIBLE EL DISPOSITIVO BLUETOOTH
@@ -116,15 +120,86 @@ void setup() {
   generarClaveInicial();
 }
 
+
+unsigned long T = 0;
+unsigned long T_Led = 0;
+unsigned long T_Direccion = 0;
+unsigned long T_Sirena = 0;
+
+bool estadoled = false;
+bool estadodireccion = true;
+bool estadosirena = true;
+
+int ciclosdireccion = 0;
+int ciclossirena = 0;
+
 void loop() {
 
   //if (analogRead(15) < 400) {
   if (digitalRead(15) == LOW) {
-    despierto = false;
-    dormir();
+    procesoApagado(1);
   }
-  delay(1000);
 
+  if (procesoParpadeo == true){
+    if (millis() >= T_Led + 150) {
+      if (estadoled == false) {
+        digitalWrite(led_rojo, HIGH);
+        estadoled = true;
+      } else {
+        digitalWrite(led_rojo, LOW);
+        estadoled = false;
+      }
+      T_Led = millis();
+    }
+  }
+
+  if (procesoEncender == true) {
+    if (millis() >= millis() + T) {
+      Serial.println("Activando Sistema");
+      T = millis();
+      procesoParpadeo = false;
+      digitalWrite(led_rojo, LOW);
+      digitalWrite(lm2596, HIGH);
+      digitalWrite(principal, HIGH);
+      digitalWrite(sirena, HIGH);
+      digitalWrite(direccion, HIGH);
+    }
+
+    if (ciclosdireccion <= 3) {
+      if (millis() >= T + T_Direccion) {
+        if (estadodireccion == true) {
+          digitalWrite(direccion, LOW);
+          estadodireccion = false;
+          ciclosdireccion += 1;
+        } else {
+          digitalWrite(direccion, HIGH);
+          estadodireccion = true;
+        }
+        T_Direccion += 300;
+      }
+    }
+
+    if (ciclossirena <= 2) {
+      if (millis() >= T + T_Sirena) {
+        if (estadosirena == true) {
+          digitalWrite(sirena, LOW);
+          estadosirena = false;
+          ciclossirena += 1;
+        } else {
+          digitalWrite(sirena, HIGH);
+          estadosirena = true;
+        }
+        T_Sirena += 200;
+      }
+    }
+    
+    if (millis() >= T + 2000) {
+      Serial.println("Encendiendo led");
+      digitalWrite(led_rojo, HIGH);
+      procesoEncender = false;
+    }
+  }
+  delay(10);
 }
 
 void dormir() {
@@ -181,9 +256,19 @@ void loop_comunicacion(void *pvParameters) {
         if (estadoActual == 0 && mensajeRecibido == "200") {
           mensajeRecibido = "";
           estadoActual = 1;
+        } else if (mensajeRecibido == "301") {
+          procesoApagado(2);
+          SerialBT.println("301Y");
+        } else if (mensajeRecibido == "302") {
+          alarma();
         } else if (mensajeRecibido.length() == 5) {
           if (validarClaveDinamica(claveDinamica, mensajeRecibido, clave[1])) {
             Serial.println("Validación Correcta");
+            if (conexionValida == false) {
+              conexionValida = true;
+              estadoActual = 2;
+              procesoEncender = true;
+            }
             contadorPaquetesPerdidos = 0;
           } else {
             Serial.println("Validación Fallida");
@@ -196,7 +281,7 @@ void loop_comunicacion(void *pvParameters) {
 
     if (estadoActual == 1) {
       GenerarClaveDinamica();
-      estadoActual = 2;
+      estadoActual = 0;
     } else if (estadoActual == 2) {
       if (millis() - tiempoAnterior >= frecuencia) {
         tiempoAnterior = millis();
@@ -207,7 +292,7 @@ void loop_comunicacion(void *pvParameters) {
         Serial.print("ContadorPerdidos: ");
         Serial.println(contadorPaquetesPerdidos);
         if(contadorPaquetesPerdidos >= 3) {
-          procesoApagado(2);
+          procesoApagado(3);
         }
         GenerarClaveDinamica();
         contador = 0;
@@ -303,7 +388,6 @@ void GenerarClaveDinamica() {
 
 bool validarClaveDinamica(long claveDinamica, String mensajeRecibido, int claveA) {
   //Serial.println("................Validacion Claves................");
-
   Serial.print("Clave Dinamica: ");
   Serial.println(claveDinamica);
   Serial.print("mensaje Recibido: ");
@@ -311,7 +395,6 @@ bool validarClaveDinamica(long claveDinamica, String mensajeRecibido, int claveA
 
   //claveDinamica = 79328;
   //claveA = 7;
-
 
   int i = 4;
   long copiaclaveDinamica = claveDinamica;
@@ -364,8 +447,82 @@ bool validarClaveDinamica(long claveDinamica, String mensajeRecibido, int claveA
 
 void procesoApagado(int tipo) {
   if (tipo == 1) {
-    Serial.println("Motivo Apagado: 1 - Desconexión");
+    Serial.println("Motivo Apagado: 1 - Switch");
+    digitalWrite(principal, LOW);
+    int contadorApagado = 0;
+    int T_LedApagdo = 0;
+    int estadoledApagado = false;
+    while (contadorApagado < 3) {
+      if (millis() >= T_LedApagdo + 200) {
+        if (estadoledApagado == false) {
+          digitalWrite(led_rojo, HIGH);
+          digitalWrite(direccion, HIGH);
+          if (contadorApagado < 2)
+            digitalWrite(sirena, HIGH);
+          estadoledApagado = true;
+        } else {
+          digitalWrite(led_rojo, LOW);
+          digitalWrite(direccion, LOW);
+          digitalWrite(sirena, LOW);
+          estadoledApagado = false;
+          contadorApagado += 1;
+        }
+        T_LedApagdo = millis();
+      }
+    }
+    digitalWrite(lm2596, LOW);
+    delay(200);
+    despierto = false;
+    dormir();
   } else if (tipo == 2) {
-    Serial.println("Motivo Apagado: 2 - Perdida de Paquetes");
+    Serial.println("Motivo Apagado: 2 - Señal de apagado remoto");
+    digitalWrite(principal, LOW);
+    int contadorApagado = 0;
+    int T_LedApagdo = 0;
+    int estadoledApagado = false;
+    while (contadorApagado < 2) {
+      if (millis() >= T_LedApagdo + 200) {
+        if (estadoledApagado == false) {
+          digitalWrite(led_rojo, HIGH);
+          digitalWrite(direccion, HIGH);
+          if (contadorApagado < 1)
+            digitalWrite(sirena, HIGH);
+          estadoledApagado = true;
+        } else {
+          digitalWrite(led_rojo, LOW);
+          digitalWrite(direccion, LOW);
+          digitalWrite(sirena, LOW);
+          estadoledApagado = false;
+          contadorApagado += 1;
+        }
+        T_LedApagdo = millis();
+      }
+    }
+  } else if (tipo == 3) {
+    Serial.println("Motivo Apagado: 3 - Desconexión, Perdida de paquetes");
+    alarma();
+  }
+}
+
+bool procesoAlarma = false;
+
+void alarma() {
+  Serial.println("Iniciando Proceso de Alarma");
+  if (procesoAlarma == false) {
+    procesoAlarma = true;
+    while (1) {
+      delay(15000);
+      digitalWrite(principal, LOW);
+      for (int i = 0; i < 40; i++) {
+        digitalWrite(led_rojo, HIGH);
+        if (i == 20)
+          digitalWrite(sirena, HIGH);
+        delay(250);
+        digitalWrite(led_rojo, LOW);
+        delay(250);
+      }
+      digitalWrite(sirena, LOW);
+      ESP.restart();
+    }
   }
 }
